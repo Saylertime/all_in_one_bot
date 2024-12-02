1a0c534f84934c223cbe88c0d771d6e100541d1a
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2024-12-02 19:29:08 +0300

    free big briefs

--- GuideGuru/handlers/default_handlers/free_texts.py
+++ GuideGuru/handlers/default_handlers/free_texts.py
@@ -13,10 +13,32 @@ def free_texts(message):
     name_in_db = find_author(username)
     if name_in_db:
         if free_authors:
-            msg = f'Сейчас свободны: \n\n' \
-                  f'{free_authors}'
+            messages = split_message_by_paragraphs(f"Сейчас свободны: \n\n{free_authors}")
+            for msg in messages:
+                bot.send_message(message.from_user.id, msg, parse_mode='Markdown', disable_web_page_preview=True)
+                msg = f'Сейчас свободны: \n\n' \
+                      f'{free_authors}'
         else:
             msg = 'Всё разобрали! Ждём новых поступлений'
     else:
         msg = f'{username}, тебя пока нет в базе данных ;( Напиши @saylertime, чтобы добавил'
     bot.send_message(message.from_user.id, msg, parse_mode='Markdown', disable_web_page_preview=True)
+
+
+def split_message_by_paragraphs(message, max_length=4000):
+    parts = []
+    current_part = ""
+
+    paragraphs = message.split("\n\n")
+
+    for paragraph in paragraphs:
+        if len(current_part) + len(paragraph) + 2 <= max_length:
+            current_part += paragraph + "\n\n"
+        else:
+            parts.append(current_part.strip())
+            current_part = paragraph + "\n\n"
+
+    if current_part.strip():
+        parts.append(current_part.strip())
+
+    return parts
--- report_bot/handlers/default_handlers/free_texts.py
+++ report_bot/handlers/default_handlers/free_texts.py
@@ -5,9 +5,31 @@ from utils.sheets import brief_is_free
 def free_texts(message):
     free_authors = brief_is_free()
     if free_authors:
-        msg = f'Сейчас свободны: \n\n' \
-              f'{free_authors}'
+        messages = split_message_by_paragraphs(f"Сейчас свободны: \n\n{free_authors}")
+        for msg in messages:
+            bot.send_message(message.from_user.id, msg, parse_mode='Markdown', disable_web_page_preview=True)
+            msg = f'Сейчас свободны: \n\n' \
+                  f'{free_authors}'
     else:
         msg = 'Ого, всё раздали! Чмаффки <333!'
 
     bot.send_message(message.chat.id, msg, parse_mode='Markdown')
+
+
+def split_message_by_paragraphs(message, max_length=4000):
+    parts = []
+    current_part = ""
+
+    paragraphs = message.split("\n\n")
+
+    for paragraph in paragraphs:
+        if len(current_part) + len(paragraph) + 2 <= max_length:
+            current_part += paragraph + "\n\n"
+        else:
+            parts.append(current_part.strip())
+            current_part = paragraph + "\n\n"
+
+    if current_part.strip():
+        parts.append(current_part.strip())
+
+    return parts
